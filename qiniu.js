de04b4b5ee89387e85f0a1d88479826847e197f6
Raffy Li <raffy2010@gmail.com>
2015-04-29 19:24:03 +0800

    some qiniu fix

--- qiniu.js
+++ qiniu.js
@@ -4,23 +4,24 @@ var Promise = require('bluebird');
 
 var config = require('./config.json');
 
-var accessKey = config.qiniu.asscee_key,
+var accessKey = config.qiniu.access_key,
     secretKey = config.qiniu.secret_key;
 
 qiniu.conf.ACCESS_KEY = accessKey;
 qiniu.conf.SECRET_KEY = secretKey;
 
 function genToken() {
-  var putPolicy = new qiniu.rs.PutPolicy({
-    scope: 'youtube-cover'
-  });
+  var putPolicy = new qiniu.rs.PutPolicy('youtube-cover');
 
   return putPolicy.token();
 }
 
 function downloadImage(url) {
   return new Promise(function(resolve, reject) {
-    request(url, function(err, httpResp, body) {
+    request({
+      url: url,
+      encoding: null
+    }, function(err, httpResp, body) {
       if (err) {
         reject(err);
 
