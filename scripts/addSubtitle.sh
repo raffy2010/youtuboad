51ca84d4110eb043bf57e8829d9492641f95a97d
Raffy Li <raffy2010@gmail.com>
2015-05-20 17:36:45 +0800

    reduce ffmpeg output video quality, always choose y

--- scripts/addSubtitle.sh
+++ scripts/addSubtitle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ffmpeg -i "$1" -f image2 -loop 1 -i "$3" -filter_complex \
+ffmpeg -y -i "$1" -f image2 -loop 1 -i "$3" -filter_complex \
   '[1:v]fade=t=in:st=1:d=1, fade=t=out:st=8:d=1[wm];[0:v][wm]overlay=x=(W-overlay_w)/2:y=H-overlay_h-20:shortest=1[outv]' \
-  -map [outv] -map 0:a -c:a copy -c:v libx264 -crf 18 "$2"
+  -map [outv] -map 0:a -c:a copy -c:v libx264 "$2"
